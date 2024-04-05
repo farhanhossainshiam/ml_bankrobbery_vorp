ef9ea4dfbc967ff33c3a9a881b1d54661674e385
farhanhossainshiam <noxkala357@gmail.com>
2024-04-05 17:18:04 +0600

    1.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,6 +4,10 @@ fx_version "adamant"
 games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+author "NoxFarhan (Discord: NoxFarhan#0001)"
+description "It's a Simple bank robbery you need to item for starting the robbery."
+version "1.0.1"
+
 server_scripts {
 	'config.lua',
 	'server/main.lua'
